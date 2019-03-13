dab1c451cc137cc16c4c1645fd28b5fb442eb68f
WaysideTester <dylanmurray211@outlook.com>
2019-03-12 20:02:36 -0500

    creating list and removing based on criteria

--- KillImagineDragons/Program.cs
+++ KillImagineDragons/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using KillImagineDragons.Songs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KillImagineDragons
 {
@@ -6,7 +9,31 @@ namespace KillImagineDragons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Song> goodSongs = new List<Song>();
+            List<Song> allSongs = new List<Song>
+            {
+                new Song("Dream Theater", "Untethered Angel"),
+                new Song("Head Above Water", "Avril Lavigne"),
+                new Song("F*** Wade", "Michael Trapson"),
+                new Song("Firelight", "Within Temptation"),
+                new Song("Giant", "Calvin Harris"),
+                new Song("Keanu Reeves", "Logic"),
+                new Song("Leave Me Lonely", "Hilltop Hoods"),
+                new Song("Meet Again", "Maxo Kream"),
+                new Song("I Was Me", "Imagine Dragons"),
+                new Song("Not Today", "Imagine Dragons"),
+                new Song("Polaroid", "Imagine Dragons"),
+                new Song("Radioactive", "Imagine Dragons")
+            };
+
+            goodSongs = allSongs.Where(song => song.Artist != "Imagine Dragons").ToList();
+
+            foreach (var song in goodSongs)
+            {
+                Console.WriteLine($"\"{song.SongName}\" is performed by {song.Artist}\n");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ KillImagineDragons/Song/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KillImagineDragons.Songs
+{
+    public class Song
+    {
+        public string Artist { get; }
+        public string SongName { get; }
+
+        public Song(string songName, string artist)
+        {
+            Artist = artist;
+            SongName = songName;
+        }
+    }
+}
